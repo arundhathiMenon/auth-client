15f8f6d217565b618488f70c518b1de67f89738a
arundhathiMenon <arundhathi@nimblebox.ai>
2024-11-18 14:25:53 +0530

    authentication

--- src/components/Authentication.jsx
+++ src/components/Authentication.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { useState } from "react";
-import ChildrenTable from "./ChildrenTable.jsx";
 import Login from "./Login.jsx";
 
 export default function Authentication() {
@@ -23,38 +22,16 @@ export default function Authentication() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 py-8">
       <h1 className="text-3xl font-bold mb-6 text-gray-800">
-        OneDrive Shared Folder Viewer
+     Ethara authentication
       </h1>
 
       {!accessToken ? (
         <Login onSuccess={handleLoginSuccess} setAccessToken={setAccessToken} />
       ) : (
         <>
-          <form
-            onSubmit={handleSubmit}
-            className="mb-6 w-full max-w-md flex flex-col items-center"
-          >
-            <input
-              type="text"
-              className="w-full px-4 py-2 mb-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-              placeholder="Enter OneDrive Shared Folder URL"
-              value={sharedFolderUrl}
-              onChange={(e) => setSharedFolderUrl(e.target.value)}
-            />
-            <button
-              type="submit"
-              className="bg-blue-500 text-white px-6 py-2 rounded-lg hover:bg-blue-600 transition"
-            >
-              Submit URL
-            </button>
-          </form>
-
-          {submittedUrl && (
-            <ChildrenTable
-              accessToken={accessToken}
-              sharedFolderUrl={submittedUrl}
-            />
-          )}
+          <h1 className="text-3xl font-bold mb-6 text-gray-800">
+            Authentication successful! You can close this window
+          </h1>
         </>
       )}
     </div>
